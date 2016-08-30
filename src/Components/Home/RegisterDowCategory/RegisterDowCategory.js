5ca324055784eb8ee440c7e553bcdc880ac74672
DESKTOP-9AHV2P6\AnhPhuong <phuongit247@gmail.com>
2016-08-30 16:35:29 +0700

    config

--- src/Components/Home/RegisterDowCategory/RegisterDowCategory.js
+++ src/Components/Home/RegisterDowCategory/RegisterDowCategory.js
@@ -3,7 +3,25 @@ export default class RegisterDowCategory extends React.Component{
   constructor(props){
     super(props);
   }
+  componentDidMount(){
+    $('#register-form').on('submit', function(e){
+      e.preventDefault();
+      $('#submit-button').css('display', 'none');
+      $('#form-loader').css('display', 'block');
+      $('#submit-text').css('display', 'none');
+      $('#submit-button').attr('disabled', 'true');
 
+      var url = 'https://script.google.com/macros/s/AKfycbzPEZPmNLtjrg4D6Ukopfo6OMQAnNgzOGmIQt1jtRjbgCePJmPH/exec';
+
+        var jqxhr = $.post(url, $('#register-form').serialize(), function(data) {
+          $('#form-loader').css('display', 'none');
+          $('#notification').css('display', 'block');
+          $('#notification').html('Bạn đã đăng ký thành công!');
+            console.log("Success! Data: " + data.statusText);
+      });
+    });
+
+  }
   render () {
     return (
       <div>
@@ -15,12 +33,12 @@ export default class RegisterDowCategory extends React.Component{
               <input type="Email" id="id_email" className="Scholarship_input" placeholder="Địa Chỉ Email..." name='Địa chỉ mail' required/>
             </div>
             <button id="submit-button" ><span  id="submit-text">Đăng Ký</span></button>
-            <h4 id="notification" className="notification"></h4>
-            <div id="form-loader" className='uil-rolling-css'>
-              <div>
-                <div></div>
-                <div></div>
-              </div>
+            <h4 id="notification" className="notification"></h4>        
+          </div>
+          <div id="form-loader" className='uil-rolling-css'>
+            <div>
+              <div></div>
+              <div></div>
             </div>
           </div>
         </form>
